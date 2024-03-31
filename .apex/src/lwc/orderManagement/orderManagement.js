2f282af63d54a9b08f3d1c01f8bf0ce0e6666095
FireG45 <cace.200218@gmail.com>
2024-03-31 16:04:36 +0300

    Added Account Name and Number Fields rendering

--- .apex/src/lwc/orderManagement/orderManagement.js
+++ .apex/src/lwc/orderManagement/orderManagement.js
@@ -1,6 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import Id from "@salesforce/user/Id";
+import { CurrentPageReference } from 'lightning/navigation';
+
 export default class orderManagement extends LightningElement {
+    @track currentPageReference;
+    @wire(CurrentPageReference)
+    setCurrentPageReference(currentPageReference) {
+        this.currentPageReference = currentPageReference;
+    }
+    userId = Id;
+    recordId;
+    accountData;
+
+    products;
+
+    loadProducts() {
+        this.products = [
+            {Name : "Product1", Description: "Description"},
+            {Name : "Product2", Description: "Description"},
+            {Name : "Product3", Description: "Description"},
+            {Name : "Product4", Description: "Description"},
+            {Name : "Product5", Description: "Description"},
+            {Name : "Product6", Description: "Description"},
+        ]
+    }
+
     connectedCallback() {
-        console.log('CALL')
+        const pageReference = this.currentPageReference;
+        this.recordId = pageReference.state.c__recordId;
+        this.accountData = pageReference.state.c__accountData;
+        this.loadProducts();
     }
 }
--- .apex/src/lwc/orderManagementButton/orderManagementButton.js
+++ .apex/src/lwc/orderManagementButton/orderManagementButton.js
@@ -1,21 +1,39 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import { NavigationMixin } from 'lightning/navigation';
-
 export default class orderManagementButton extends NavigationMixin(LightningElement) {
+    @api accountId;
+    accountData;
+
+    @wire(getRecord, { recordId: '$accountId', fields: [ACCOUNT_OBJECT.Name, ACCOUNT_OBJECT.AccountNumber] })
+    account;
+
+    get accountName() {
+        return this.account.data ? getFieldValue(this.account.data, ACCOUNT_OBJECT.Name) : '';
+    }
+
+    get accountNumber() {
+        return this.account.data ? getFieldValue(this.account.data, ACCOUNT_OBJECT.AccountNumber) : '';
+    }
 
     connectedCallback() {
+        this.accountData = {name : this.accountName, number : this.accountNumber}
         this.orderManagementPageRef = {
             type: 'standard__component',
             attributes: {
                 componentName: 'c__orderManagementWrapper'
             },
+            state: {
+                c__recordId: this.accountId,
+                c__accountData: this.accountData,
+            }
         };
     }
 
     handleClick(evt) {
         evt.preventDefault();
         evt.stopPropagation();
-
         this[NavigationMixin.GenerateUrl](this.orderManagementPageRef).then(url => { window.open(url) })
     }
 }
